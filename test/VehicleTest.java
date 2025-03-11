2d94d9d9c4a7d5cc99bd80801d9b4c3dbe6cf773
mansonchoi <114096473+mansonchoi@users.noreply.github.com>
2025-03-10 21:35:47 -0400

    Update VehicleTest.java

--- test/VehicleTest.java
+++ test/VehicleTest.java
@@ -1,4 +1,15 @@
-public class VehicleTest {
+public class VehicleTest 
+{
+    @Test
+    public void testVehicleCreation() 
+    {
+        Vehicle vehicle = new Vehicle("Toyota", "Corolla", 2022, "Gasoline", "images/corolla.jpg", "Reliable.");
 
-    
+        assertEquals("Toyota", vehicle.getMake());
+        assertEquals("Corolla", vehicle.getModel());
+        assertEquals(2022, vehicle.getYear());
+        assertEquals("Gasoline", vehicle.getFuelType());
+        assertEquals("images/corolla.jpg", vehicle.getImageUrl());
+        assertEquals("Reliable.", vehicle.getReview());
+    }
 }
