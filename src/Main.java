ff0f4ab9d5729c2c3d386a8fdc3022fb12fe277d
rahuls1213 <rahul.saravanan1@ontariotechu.net>
2025-03-17 16:12:44 -0400

    Updated CarViewer.java to include favourites page and compare cars side-by-side dynamically

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            //Modern look by using Nimbus
             UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        javax.swing.SwingUtilities.invokeLater(() -> new CarViewer().setVisible(true));
+        VehicleDatabase.loadVehicles();
+        javax.swing.SwingUtilities.invokeLater(() -> new CarViewer(VehicleDatabase.getVehicles()));
     }
 }
