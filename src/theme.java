e97a7f26d5babe17184fbb35265d5b240433fcdc
mansonchoi <114096473+mansonchoi@users.noreply.github.com>
2025-03-24 21:34:48 -0400

    Update theme.java

--- src/theme.java
+++ src/theme.java
@@ -3,8 +3,10 @@ import java.awt.*;
 
 public class DarkGreenUI {
     public static void applyDarkGreenTheme(JFrame frame) {
+
         Color darkGreen = new Color(34, 45, 40); 
-        Color lightGreen = new Color(50, 80, 60); 
+        Color black = new Color(20, 25, 22); 
+        Color lightGreen = new Color(50, 120, 70); 
         Color textColor = Color.WHITE;
         
         frame.getContentPane().setBackground(darkGreen);
@@ -13,12 +15,17 @@ public class DarkGreenUI {
             if (c instanceof JLabel || c instanceof JButton) {
                 c.setForeground(textColor);
                 c.setBackground(lightGreen);
+            } else if (c instanceof JPanel) {
+                c.setBackground(black);
             }
         }
-
-        UIManager.put("Panel.background", darkGreen);
+        
+        UIManager.put("Panel.background", black);
         UIManager.put("Button.background", lightGreen);
         UIManager.put("Button.foreground", textColor);
         UIManager.put("Label.foreground", textColor);
+        UIManager.put("TextField.background", darkGreen);
+        UIManager.put("TextField.foreground", textColor);
+        UIManager.put("TextField.caretForeground", textColor);
     }
 }
